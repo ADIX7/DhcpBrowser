d453533e3974756ab0deddc7117f7eccd7d79bc2
Ádám Kovács <kovacsadam07@outlook.hu>
2024-05-28 07:52:15 +0200

    Use current url as fallback on UI, .env file ignores

--- frontend/src/di.ts
+++ frontend/src/di.ts
@@ -4,5 +4,5 @@ import { BackendApi } from "./services/backend-api";
 
 console.log(import.meta.env);
 
-container.register(BackendApiSettings, { useValue: new BackendApiSettings((window as any).BACKEND_API_URL || import.meta.env.VITE_BACKEND_API_URL || "") });
+container.register(BackendApiSettings, { useValue: new BackendApiSettings((window as any).BACKEND_API_URL ?? import.meta.env.VITE_BACKEND_API_URL ?? window.location.origin) });
 container.register(BackendApi, { useClass: BackendApi }, { lifecycle: Lifecycle.Singleton });
